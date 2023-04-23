ff3080e1c7bb734b3ccca36475655b699f2a06e6
hityangsir <hityangsir@ufl.edu>
2023-04-23 15:43:32 -0400

    checkin runner

--- run_multigpu.sh
+++ run_multigpu.sh
@@ -2,6 +2,7 @@
 
 # run this DDP code with multiGPUs on a single Node
 # 1. request resources from interactive mode or OOD
+
  
-conda activate pytorch/1.10
+module load pytorch/1.10
 python multigpu.py 50 10
--- run_multigpu_torchrun.sh
+++ run_multigpu_torchrun.sh
@@ -3,6 +3,6 @@
 # run this DDP code with multiGPUs on a single Node
 # 1. request resources from interactive mode or OOD
  
-conda activate pytorch/1.10
+module load pytorch/1.10
 
-torchrun --standalone --nproc_per_node=gpu multigpu_torchrun.py 50 10
+torchrun --standalone --nproc_per_node=2 multigpu_torchrun.py 50 10
